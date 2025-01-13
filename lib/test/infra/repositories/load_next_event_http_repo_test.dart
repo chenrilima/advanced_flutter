ecdc76526c2bea8f46b502b1d3b235d1c2909cba
Carlos Henrique <chenrilima.ch@gmail.com>
2025-01-13 10:21:31 -0300

    ensure LoadNextEventHttpRepository throws unexpected error on 400

--- lib/test/infra/repositories/load_next_event_http_repo_test.dart
+++ lib/test/infra/repositories/load_next_event_http_repo_test.dart
@@ -12,6 +12,8 @@ import 'package:advanced_flutter/test/helpers/fakes.dart';
 
 import '../../../domain/repositories/load_next_event_repo.dart';
 
+enum DomainError { unexpected }
+
 class LoadNextEventHttpRepository implements LoadNextEventRepository {
   final Client httpClient;
   final String url;
@@ -28,6 +30,9 @@ class LoadNextEventHttpRepository implements LoadNextEventRepository {
       'accept': 'application/json'
     };
     final response = await httpClient.get(uri, headers: headers);
+    if (response.statusCode == 400) {
+      throw DomainError.unexpected;
+    }
     final event = jsonDecode(response.body);
     return NextEvent(
         groupName: event['groupName'],
@@ -177,5 +182,11 @@ void main() {
     expect(event.players[1].name, 'name 2');
     expect(event.players[1].confirmationDate, DateTime(2024, 8, 29, 11, 0));
     expect(event.players[1].isConfirmed, false);
+  });
+
+  test('Should throw UnexpectedError on 400', () async {
+    httpClient.statusCode = 400;
+    final future = sut.loadNextEvent(groupId: groupId);
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
